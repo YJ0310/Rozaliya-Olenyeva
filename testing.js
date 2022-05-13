a815d89f4e54baef2ac74d273592173e4c12fe31
YJ0310 <yinjiasek@gmail.com>
2022-05-13 15:09:34 +0800

    update files

--- testing.js
+++ testing.js
@@ -5,7 +5,6 @@ const { temp_channel_id } = require("./src/function/voice_channel_check");
 const { MessageButton, MessageActionRow, Interaction, MessageAttachment } = require("discord.js");
 const { guild_f } = require("./src/function/guild");
 const Canva = require(`canvas`);
-const { message_embed } = require("./src/function/message_embed");
 const testing_channel_id = `958279724369592320`;
 
 module.exports = async () => {
